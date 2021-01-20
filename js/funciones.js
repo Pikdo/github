3fd3982cd5131dbab3b60b87a486ed09cd504d6d
Pikdo <ali87to@gmail.com>
2021-01-20 16:04:21 -0600

    Cambios en espaciado de funciones.js

--- js/funciones.js
+++ js/funciones.js
@@ -1,12 +1,8 @@
 (()=>{
-
     fetch("comandos.txt")
     .then(response => response.text())
     .then(data => {
         let main = document.getElementById('notas');
         main.innerText = data;
     });
-   
-    
-
 })();
