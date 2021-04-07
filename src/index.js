d7d88971cee105b67704c06aed7fe3db13627faf
Pikdo <ali87to@gmail.com>
2021-04-07 11:28:01 -0600

    Responsive design mobile first

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 console.log("Hola mundo");
+
+(()=>{
+    fetch("comandos.txt")
+    .then(response => response.text())
+    .then(data => {
+        let main = document.getElementById('notas');
+        main.innerText = data;
+    });
+})();
